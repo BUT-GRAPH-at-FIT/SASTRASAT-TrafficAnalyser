a157b22b57761dbac6ae84e9df0fc69261af8a9d
Šimon Strýček <xstryc06@stud.fit.vutbr.cz>
2025-07-04 15:44:21 +0200

    Add dynamic detector scale factor

--- libsj/nn/object_detector.py
+++ libsj/nn/object_detector.py
@@ -46,8 +46,8 @@ class ObjectDetector:
         assert frame.ndim == 3
         # speed up detection by scaling
         if self.scale_factor is not None:
-            if frame.shape[0] * self.scale_factor > 300:
-                self.scale_factor = 300 / frame.shape[0]
+            if self.scale_factor > 1:
+                self.scale_factor = self.scale_factor / frame.shape[0]
 
             frame = cv2.resize(frame,(0,0),fx=self.scale_factor, fy=self.scale_factor, interpolation=cv2.INTER_AREA)
         if threshold is None:
