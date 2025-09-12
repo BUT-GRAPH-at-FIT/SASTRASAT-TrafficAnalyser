8a842efbe6ec255890a773c9f2016f9d2cdf85c9
Šimon Strýček <istryc@vutbr.cz>
2025-09-12 11:43:43 +0200

    Enhance matching function speed and error handling

--- matching/tools.py
+++ matching/tools.py
@@ -1,35 +1,40 @@
+"""
+Author: Simon Strycek
+Date: 2025-09-12
+Description: Tools for car matching.
+"""
+
 import numpy as np
 import faiss
 import math
 
-def find_matches(query: np.ndarray, db: np.ndarray | faiss.IndexIVFFlat, emb_size: int = 128, batch_size: int = 10, move_to_gpu: bool = True):
+
+def find_matches(query: np.ndarray, db: np.ndarray | faiss.IndexIVFFlat, batch_size: int = 10, move_to_gpu: bool = True):
     if isinstance(db, faiss.IndexIVFFlat):
+        emb_size = db.d
         index = db
     elif isinstance(db, np.ndarray):
         nlist = int(math.sqrt(len(db)))
+        emb_size = db.shape[1]
 
         quantizer = faiss.IndexFlatIP(emb_size)
         index = faiss.IndexIVFFlat(quantizer, emb_size, nlist, faiss.METRIC_INNER_PRODUCT)
-
+        
+        db = np.ascontiguousarray(db.astype(np.float32, copy=False))
         index.train(db)
         index.add(db)
-    else:
-        raise ValueError("`db` must be either a numpy array or a `faiss.IndexIVFFlat`.")
+
+    if query.shape[1] != emb_size:
+        raise ValueError(f"The number of features in query ({query.shape[1]}) and db ({emb_size}) must match.")
 
     index.nprobe = 10 # empirically chosen
 
-    if not move_to_gpu:
+    if move_to_gpu:
         index = faiss.index_cpu_to_gpu(
             faiss.StandardGpuResources(), device=0, index=index
         )
 
-    similarities, indices = [], []
+    query = np.ascontiguousarray(query.astype(np.float32, copy=False))
+    similarities, indices = index.search(query, 1)  # TOP1 per query
 
-    n_queries = len(query)
-    for idx, batch_start in enumerate(range(0, n_queries, batch_size)):
-        batch_end = min(batch_start + batch_size, n_queries)
-        sim, ind = index.search(query[batch_start:batch_end], 1) # find TOP1
-        similarities.extend(sim.flatten().tolist())
-        indices.extend(ind.flatten().tolist())
-
-    return similarities, indices
+    return similarities.flatten().tolist(), indices.flatten().tolist()
